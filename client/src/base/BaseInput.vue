b7c8851e588610f4b352619b72eb7e6a552e7155
SamanKeighobadi <saman.keighobadi@gmail.com>
2025-08-06 22:35:59 +0330

    feat: create BaseInput Components

---
+++ client/src/base/BaseInput.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { DaisyUIColors, DaisyUISizes } from '@/types/ui.types';
+import { computed } from 'vue';
+
+type InputType = "text" | "password" | "email"
+
+interface InputProps {
+    color?: DaisyUIColors;
+    size?: DaisyUISizes;
+    type?: InputType;
+    placeholder?: string
+    disabled?: boolean
+}
+
+const { color, placeholder, size, type } = defineProps<InputProps>()
+
+const inputClasses = computed(() => ({
+    "input": true,
+    [`input-${color}`]: color,
+    [`input-${size}`]: size,
+
+}))
+
+</script>
+
+<template>
+
+    <input :type="type" :placeholder="placeholder" :class="inputClasses" :disabled="disabled">
+</template>
---
+++ client/src/types/ui.types.ts
@@ -0,0 +1,2 @@
+export type DaisyUIColors = "neutral" | "primary" | "info" | "error" | "secondary" | "accent" | "success"
+export type DaisyUISizes = 'xs' | 'sm' | 'md' | 'lg';
