0ad83b54aac91934dc3f77942b88cceff3391de4
SamanKeighobadi <saman.keighobadi@gmail.com>
2025-08-07 12:29:30 +0330

    feat: create BaseTextarea component

---
+++ client/src/base/BaseTextarea.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { DaisyUIColors, DaisyUISizes } from '@/types/ui.types';
+import { computed } from 'vue';
+
+
+interface TextareaProps {
+    color?: DaisyUIColors;
+    size?: DaisyUISizes;
+    placeholder?: string
+    disabled?: boolean;
+    rows?:number;
+    cols?:number
+}
+
+const { color, placeholder, size} = defineProps<TextareaProps>()
+
+
+const classes = computed(() => ({
+    'textarea': true,
+    [`textarea-${color}`]: color,
+    [`textarea-${size}`]: size
+}))
+
+</script>
+
+<template>
+    <textarea :placeholder="placeholder" :class="classes" :disabled="disabled" :rows="rows" :cols="cols"></textarea>
+</template>
